5404cbf458a5e234810f6803c6466d184c1909a1
Diego López Naujokat <diego.l@uteca.edu.mx>
2024-02-15 08:44:44 -0600

    1er commit de nodos

---
+++ lib/classes.py
@@ -0,0 +1,12 @@
+class nodo():
+    def __init__(self, valor):
+        self.valor = valor
+        self.left = None
+        self.right = None
+        pass
+    
+    def __str__(self):
+        return f"Valor: {self.valor}, Izq: {self.left}, Der: {self.right}"
+    
+    
+    pass
---
+++ main.py
@@ -0,0 +1,16 @@
+from lib import *
+
+print("")
+
+nodo1=nodo(2)
+nodo2=nodo(10)
+nodo3=nodo(5)
+
+nodo1.left = nodo3
+nodo1.right = nodo2
+
+print(nodo1)
+print(nodo2)
+print(nodo3)
+
+print("")
